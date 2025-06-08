9af53ccbaeed2776873403dd28544d3bea947eaa
Azar <azar.h742@gmail.com>
2025-06-09 01:44:38 +0200

    charging station to one

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ step = 0
 counter = 0
 charging_vehicles = []
 traci_connection = traci.connect(port=sumo_port)
+direction_flag = True
 while step < 2000:
     traci_connection.simulationStep()
     for vehicle_id in traci_connection.vehicle.getIDList():
@@ -31,42 +32,37 @@ while step < 2000:
             )
         )
         if current_battery <= vehicle_battery_capcity * 0.15:
-            if traci_connection.vehicle.getPosition(vehicle_id)[0] < -2:
-                charging_cars_station_1 = (
-                    traci_connection.chargingstation.getVehicleIDs("cs_0")
-                )
-                charging_cars_station_2 = (
-                    traci_connection.chargingstation.getVehicleIDs("cs_1")
-                )
-                selected_station_id = (
-                    "cs_0"
-                    if len(charging_cars_station_1) < len(charging_cars_station_2)
-                    else "cs_1"
-                )
-                if selected_station_id == "cs_0":
-                    traci_connection.vehicle.setRoute(vehicle_id, ["E0", "E1", "E2", "E4"])
-                else:
-                    traci_connection.vehicle.setRoute(vehicle_id, ["E0", "E6", "E7", "E8"])
-                    
-                traci_connection.vehicle.setChargingStationStop(
-                    vehicle_id,
-                    selected_station_id,
-                    duration=600,
-                    until=1000,
-                )
+            if traci_connection.vehicle.getPosition(vehicle_id)[0] < 55:
+                try:
+                    selected_station_id = "cs_0"
+                    traci_connection.vehicle.setRoute(
+                        vehicle_id, ["E3", "E6", "E7", "E8"]
+                    )
+                    # else:
+                    #     traci_connection.vehicle.setRoute(
+                    #         vehicle_id, ["E0", "E6", "E7", "E8"]
+                    #     )
+
+                    traci_connection.vehicle.setChargingStationStop(
+                        vehicle_id,
+                        selected_station_id,
+                    )
+                    direction_flag = not direction_flag
+
+                except:
+                    print("error in setting routes")
 
             charging_cars_ids = traci_connection.chargingstation.getVehicleIDs("cs_0")
-            charging_cars_ids = (
-                charging_cars_ids
-                + traci_connection.chargingstation.getVehicleIDs("cs_1")
-            )
-
+            # charging_cars_ids = (
+            #     charging_cars_ids
+            #     + traci_connection.chargingstation.getVehicleIDs("cs_1")
+            # )
             for v_id in charging_cars_ids:
 
                 current_battery = float(
                     traci_connection.vehicle.getParameter(
                         v_id,
-                        "device.battery.chargeLevel",
+                        "device.battery.actualBatteryCapacity",
                     )
                 )
                 vehicle_battery_capcity = float(
@@ -74,11 +70,11 @@ while step < 2000:
                         v_id, "device.battery.capacity"
                     )
                 )
-                print(
-                    f"battery:{current_battery}, capacity: {vehicle_battery_capcity},Vehicle: {v_id}"
-                )
-                if current_battery >= vehicle_battery_capcity * 0.8:
-                    print(f"vehicle with id {v_id} was charged.")
+                # print(
+                #     f"battery:{current_battery}, capacity: {vehicle_battery_capcity},Vehicle: {v_id}"
+                # )
+                if current_battery >= vehicle_battery_capcity * 0.3:
+                    print(f"vehicle with id {v_id} was charged.{current_battery}")
                     traci_connection.vehicle.resume(v_id)
             # print(f"must recharge.{counter}")
     step += 1
